e8abe171661254d2c5bd30d0e08a63776279d88a
manuo-git <saintmanuo@gmail.com>
2026-07-07 01:15:10 +0900

    ビルド時にバックアップファイルも同様にアップデートするように処理変更

--- build_snippets.py
+++ build_snippets.py
@@ -6,26 +6,33 @@ def generate_snippets():
     # スクリプトの場所を基準に絶対パスを計算
     base_dir = Path(__file__).parent
     src_dir = base_dir / "src"
+
+    snippets = make_snippets(src_dir)
     
     # 🌟 config.json からパスを読み込む
     config_file = base_dir / "config.json"
-    output_dir = None
+    vscode_snippet_dir = None
     
     if config_file.exists():
         with open(config_file, "r", encoding="utf-8") as cf:
             config = json.load(cf)
             # JSONからパスを取得して Path オブジェクトに変換
             if "vscode_snippets_dir" in config:
-                output_dir = Path(config["vscode_snippets_dir"])
+                vscode_snippet_dir = Path(config["vscode_snippets_dir"])
 
-    if not output_dir or not output_dir.exists():
-        print("Warning: config.json が見つからないか、パスが無効です。リポジトリ内に保存します。")
-        output_dir = base_dir / "snippets"
-        output_dir.mkdir(exist_ok=True)
-        
-    output_file = output_dir / "python.json"
+    if not vscode_snippet_dir or not vscode_snippet_dir.exists():
+        print("Warning: config.json が見つからないか、パスが無効です。")
+    else:
+        output_file = vscode_snippet_dir / "python.json"
+        write_to_file(output_file, snippets)
+    backup_snippet_dir = base_dir / "snippets"
+    backup_snippet_dir.mkdir(exist_ok=True)
+    output_file = backup_snippet_dir / "python.json"
+    write_to_file(output_file, snippets)
+
+def make_snippets(src_dir):
     snippets = {}
-    
+
     for file_path in src_dir.glob("*.py"):
         with open(file_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
@@ -56,7 +63,9 @@ def generate_snippets():
             "body": code_lines
         }
         print(f"Loaded: {name} (file: {file_path.name})")
+    return snippets
 
+def write_to_file(output_file, snippets):
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(snippets, f, indent=4, ensure_ascii=False)
     
