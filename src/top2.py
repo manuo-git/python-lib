98a86ed10874d082cfe1341b849ed34a24b6b6c1
manuo-git <saintmanuo@gmail.com>
2026-08-14 13:27:12 +0900

    Top2に使用例を追加。_update()を内部関数にして+=でmergeする方針に

--- src/top2.py
+++ src/top2.py
@@ -1,16 +1,18 @@
 # name: Top2
 # prefix: top2
 # ---
-from typing import ClassVar
+NEG_INF = -(1<<60)
 class Top2:
-    NEG_INF: ClassVar[int] = -(1<<60)
     mx1: int
     mx2: int
-    def __init__(self):
-        self.mx1 = Top2.NEG_INF
-        self.mx2 = Top2.NEG_INF
+    multi: bool
+    def __init__(self, multi: bool = True):
+        self.mx1 = NEG_INF
+        self.mx2 = NEG_INF
+        self.multi = multi
     
     def insert(self, x: int):
+        if not self.multi and (self.mx1 == x or self.mx2 == x): return
         if self.mx1 < x:
             self.mx2 = self.mx1
             self.mx1 = x
@@ -23,25 +25,43 @@ class Top2:
         if i == 1: return self.mx2
 
     def __iter__(self):
-        if self.mx1 != Top2.NEG_INF: yield self.mx1
-        if self.mx2 != Top2.NEG_INF: yield self.mx2
+        if self.mx1 != NEG_INF: yield self.mx1
+        if self.mx2 != NEG_INF: yield self.mx2
     
     def __len__(self):
-        if self.mx1 == Top2.NEG_INF: return 0
-        if self.mx2 == Top2.NEG_INF: return 1
+        if self.mx1 == NEG_INF: return 0
+        if self.mx2 == NEG_INF: return 1
         return 2
     
     def __repr__(self):
-        vals = [str(x) if x != Top2.NEG_INF else "-INF" for x in [self.mx1, self.mx2]]
+        vals = [str(x) if x != NEG_INF else "-INF" for x in [self.mx1, self.mx2]]
         return f"Top2({', '.join(vals)})"
     
-    def update(self, other):
+    def _update(self, other):
         if other.mx1 > self.mx2: self.insert(other.mx1)
         if other.mx2 > self.mx2: self.insert(other.mx2)
     
     def __add__(self, other):
-        res = Top2()
+        res = Top2(self.multi)
         res.mx1 = self.mx1
         res.mx2 = self.mx2
-        res.update(other)
+        res._update(other)
         return res
+"""
+# 重複を許す場合
+a = Top2()
+a.insert(5) # 5, -INF
+a.insert(3) # 5, 3
+a.insert(2) # 5, 3
+a.insert(4) # 5, 4
+a.insert(5) # 5, 5
+a.insert(6) # 6, 5
+# 重複を許さない場合
+b = Top2(False)
+a.insert(5) # 5, -INF
+a.insert(3) # 5, 3
+a.insert(2) # 5, 3
+a.insert(4) # 5, 4
+a.insert(5) # 5, 4 <-
+a.insert(6) # 6, 5
+"""
